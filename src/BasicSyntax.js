d78a48cbb7247324142e589fe3dffbffdea7b272
Иван Шульженко <ilitetop@yandex.ru>
2022-03-24 00:17:55 +0700

    Базовый синтаксис

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,75 @@
 export function romanToInteger(str) {
     let result = 0;
+    let lastChar = '';
     /*
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+
+    for (let i = 0; i < str.length; i++) {
+        switch (str.substr(i, 1)) {
+            case 'I':
+                result += 1;
+                lastChar = 'I';
+
+                break;
+            case 'V':
+                result += 5;
+
+                if (lastChar === 'I') {
+                    result -= 1 * 2;
+                    lastChar = '';
+                }
+
+                break;
+            case 'X':
+                result += 10;
+
+                if (lastChar === 'I') {
+                    result -= 1 * 2;
+                    lastChar = '';
+                }
+
+                lastChar = 'X';
+                break;
+            case 'L':
+                result += 50;
+
+                if (lastChar === 'X') {
+                    result -= 10 * 2;
+                    lastChar = '';
+                }
+
+                break;
+            case 'C':
+                result += 100;
+
+                if (lastChar === 'X') {
+                    result -= 10 * 2;
+                    lastChar = '';
+                }
+
+                lastChar = 'C';
+                break;
+            case 'D':
+                result += 500;
+
+                if (lastChar === 'C') {
+                    result -= 100 * 2;
+                    lastChar = '';
+                }
+
+                break;
+            case 'M':
+                result += 1000;
+
+                if (lastChar === 'C') {
+                    result -= 100 * 2;
+                    lastChar = '';
+                }
+
+                break;
+        }
+    }
     return result;
 }
